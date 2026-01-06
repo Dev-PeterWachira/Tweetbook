abe52955bb7d082d995d6921d5e47655ce04c298
PeterMainaWachira <PeterMainaWachira@JITU-PC0XLHKX>
2026-01-06 23:46:46 +0300

    Add project files.

---
+++ Tweetbook/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tweetbook.Controllers
+{
+    public class TestController : ControllerBase
+    {
+        [HttpGet("api/user")]
+
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Nick" });
+        }
+    }
+}
---
+++ Tweetbook/Options/SwaggerOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Tweetbook
+{
+
+
+    public class SwaggerOptions
+    {
+        public string JsonRoute { get; set; }
+        public string UiEndPoint { get; set; }  // Define UiEndPoint property
+        public string Description { get; set; }
+    }
+}
+
